a8a0fc5ed8f2afedd84e63b98c3480f51dabed3c
nataprawiraf <nataprawiraf@gmail.com>
2020-02-10 09:12:19 +0700

    finalize

--- main.py
+++ main.py
@@ -7,10 +7,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    cat_image = requests.get('https://api.thecatapi.com/v1/images/search', headers={"x-api-key":"9d3b38de-757e-41ca-b77a-603d16f52442"}).json()
-    cat_breeds = requests.get('https://api.thecatapi.com/v1/breeds', headers={"x-api-key":"9d3b38de-757e-41ca-b77a-603d16f52442"}).json()
-    pp(cat_image)
-    return render_template('fact.html', cat_image=cat_image[0], cat_breeds=cat_breeds)
+    cat_image = requests.get('https://api.thecatapi.com/v1/images/search', 
+                headers={"x-api-key":"9d3b38de-757e-41ca-b77a-603d16f52442"}).json()
+    cat_fact = requests.get('https://cat-fact.herokuapp.com/facts/random?animal_type=cat').json()
 
+    return render_template('fact.html', 
+        cat_image=cat_image[0],
+        cat_fact=cat_fact,
+    )
+
+    # cat_breeds = requests.get('https://api.thecatapi.com/v1/breeds', 
+    #             headers={"x-api-key":"9d3b38de-757e-41ca-b77a-603d16f52442"}).json()
 if __name__=='__main__':
     app.run(host='0.0.0.0', port=20800, debug=True)
